da69878ed980fc160547f13c83f340e62b3f9a1a
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-02-02 01:31:49 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+export default function page() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="text-center font-semibold font-Outfit ">
+      <h1>Party Pals</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </main>
   );
