6706897bb7e7d0f349c4e40561f4fa5bf0af4c0d
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-02-23 15:34:26 +0100

    .

--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -5,7 +5,7 @@ import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/utils/utils"
-import { Button } from "@/components/ui/Button"
+import  Button  from "../ui/Button"
 
 const CarouselContext = React.createContext(null)
 
--- src/components/ui/CarouselPlugin.jsx
+++ src/components/ui/CarouselPlugin.jsx
@@ -8,7 +8,7 @@ import {
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "../ui/Carousel"
 
 export function CarouselPlugin({ children }) { 
     const plugin = React.useRef(
