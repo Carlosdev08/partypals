c9fd513462f376b4b147de0efc96367721fb4b13
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-03-01 21:33:00 +0100

    Update route.js

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const admin = require('firebase-admin');
+// require('dotenv').config();
+// const admin = require('firebase-admin');
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-};
+// const serviceAccount = {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+// };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
 
-module.exports = admin;
+// module.exports = admin;
